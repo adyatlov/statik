153a7e4224d707e422b013703ddfe4bf9da24d54
Andrey Dyatlov <adyatlov@posteo.net>
2024-09-26 09:19:55 +0200

    Fix domain

--- cmd/main.go
+++ cmd/main.go
@@ -122,7 +122,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "1d08eb77.example.com",
+		LeaderElectionID:       "1d08eb77.dyatlov.net",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
